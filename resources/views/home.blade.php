0816b2a03ab28f3d368517fb87183d916a41ef3e
odetv <abdiutama02@gmail.com>
2026-02-02 20:36:14 +0800

    initial commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html>
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    @vite('resources/css/app.css')
+    <title>SPPI Buleleng</title>
+  </head>
+  <body>
+    <h1 class="text-3xl font-bold">
+        Halo, SPPI Buleleng!
+    </h1>
+  </body>
+</html>
